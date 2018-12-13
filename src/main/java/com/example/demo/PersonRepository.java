c0dcfc51b5f4d65498b16606bddf563dd81dd4e1
Jens Schauder <jschauder@pivotal.io>
2018-12-13 14:56:57 +0100

    reproduced issue independently of Spring Data.

--- src/main/java/com/example/demo/PersonRepository.java
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -8,13 +8,13 @@ import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 
-    @Query(value = "SELECT p.* FROM person p WHERE p.id = :#{#id}", nativeQuery = true)
+    @Query(value = "SELECT p.* FROM person p WHERE p.id = :id", nativeQuery = true)
     Person select(@Param("id") UUID id);
 
-    @Query(value = "SELECT p.name FROM person p WHERE p.id = :#{#id}", nativeQuery = true)
+    @Query(value = "SELECT p.name FROM person p WHERE p.id = :id", nativeQuery = true)
     PersonName selectName(@Param("id") UUID id);
 
-    @Query(value = "SELECT p.id FROM person p WHERE p.id = :#{#id}", nativeQuery = true)
+    @Query(value = "SELECT p.id FROM person p WHERE p.id = :id", nativeQuery = true)
     PersonId selectId(@Param("id") UUID id);
 
 }
--- src/test/java/com/example/demo/PersonRepositoryTest.java
+++ src/test/java/com/example/demo/PersonRepositoryTest.java
@@ -13,6 +13,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
 import java.util.UUID;
 
 @DataJpaTest
@@ -20,72 +22,133 @@ import java.util.UUID;
 @ContextConfiguration(initializers = {PersonRepositoryTest.Initializer.class})
 class PersonRepositoryTest {
 
-    private static final PostgreSQLContainer DB = new PostgreSQLContainer("postgres:latest");
+	private static final PostgreSQLContainer DB = new PostgreSQLContainer("postgres:latest");
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + DB.getJdbcUrl(),
-                    "spring.datasource.username=" + DB.getUsername(),
-                    "spring.datasource.password=" + DB.getPassword()
-            ).applyTo(applicationContext);
-        }
-    }
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		@Override
+		public void initialize(ConfigurableApplicationContext applicationContext) {
+			TestPropertyValues.of(
+					"spring.datasource.url=" + DB.getJdbcUrl(),
+					"spring.datasource.username=" + DB.getUsername(),
+					"spring.datasource.password=" + DB.getPassword()
+			).applyTo(applicationContext);
+		}
+	}
 
-    @BeforeAll
-    static void beforeAll() {
-        DB.start();
-    }
+	@BeforeAll
+	static void beforeAll() {
+		DB.start();
+	}
 
-    @AfterAll
-    static void afterAll() {
-        DB.stop();
-    }
+	@AfterAll
+	static void afterAll() {
+		DB.stop();
+	}
 
-    @Autowired
-    private PersonRepository personRepository;
+	@Autowired
+	private PersonRepository personRepository;
 
-    @Test
-    void findById() {
-        // Given
-        Person p = new Person();
-        p.setName("Mickey");
-        UUID id = personRepository.save(p).getId();
+	@Autowired
+	private EntityManager entityManager;
 
-        // When
-        Person person = personRepository.select(id);
+	@Test
+	void findById() {
+		// Given
+		Person p = new Person();
+		p.setName("Mickey");
+		UUID id = personRepository.save(p).getId();
 
-        // Then
-        Assertions.assertEquals(id, person.getId());
-        Assertions.assertEquals("Mickey", person.getName());
-    }
+		// When
+		Person person = personRepository.select(id);
 
-    @Test
-    void selectName() {
-        // Given
-        Person p = new Person();
-        p.setName("Donald");
-        UUID id = personRepository.save(p).getId();
+		// Then
+		Assertions.assertEquals(id, person.getId());
+		Assertions.assertEquals("Mickey", person.getName());
+	}
 
-        // When
-        PersonName personName = personRepository.selectName(id);
+	@Test
+	void selectName() {
+		// Given
+		Person p = new Person();
+		p.setName("Donald");
+		UUID id = personRepository.save(p).getId();
 
-        // Then
-        Assertions.assertEquals("Donald", personName.getName());
-    }
+		// When
+		PersonName personName = personRepository.selectName(id);
 
-    @Test
-    void selectId() {
-        // Given
-        Person p = new Person();
-        p.setName("Daisy");
-        UUID id = personRepository.save(p).getId();
+		// Then
+		Assertions.assertEquals("Donald", personName.getName());
+	}
 
-        // When
-        PersonId personId = personRepository.selectId(id);
+	@Test
+	void selectId() {
+		// Given
+		Person p = new Person();
+		p.setName("Daisy");
+		UUID id = personRepository.save(p).getId();
 
-        // Then
-        Assertions.assertEquals(id, personId.getId());
-    }
+		// When
+		PersonId personId = personRepository.selectId(id);
+
+		// Then
+		Assertions.assertEquals(id, personId.getId());
+	}
+
+
+	@Test
+	void selectJpaAll() {
+
+		Person p = new Person();
+		p.setName("Daisy");
+		UUID id = personRepository.save(p).getId();
+
+		UUID reloadedId = entityManager.createQuery("select id from Person ", UUID.class).getSingleResult();
+
+		Assertions.assertEquals(id, reloadedId);
+	}
+
+	@Test
+	void selectJpaId() {
+
+		Person p = new Person();
+		p.setName("Daisy");
+		UUID id = personRepository.save(p).getId();
+
+		UUID reloadedId = entityManager //
+				.createQuery("select id from Person where id = :id", UUID.class) //
+				.setParameter("id", id)
+				.getSingleResult();
+
+		Assertions.assertEquals(id, reloadedId);
+	}
+
+
+	@Test
+	void selectJpaNoTargetClass() {
+
+		Person p = new Person();
+		p.setName("Daisy");
+		UUID id = personRepository.save(p).getId();
+
+		Object reloadedId = entityManager //
+				.createQuery("select id from Person") //
+				.getSingleResult();
+
+		Assertions.assertEquals(id, reloadedId);
+	}
+
+	@Test
+	void selectNativeJpaNoTargetClass() {
+
+		Person p = new Person();
+		p.setName("Daisy");
+		UUID id = personRepository.save(p).getId();
+
+		Object reloadedId = entityManager //
+				.createNativeQuery("select id from Person") //
+				.getSingleResult();
+
+		Assertions.assertEquals(id, reloadedId);
+	}
+
 }
